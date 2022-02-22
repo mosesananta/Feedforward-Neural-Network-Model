69e97cb1b4e29c604547941761dc737d9a0f4214
Moses Ananta <moses.ananta@gmail.com>
2022-02-22 14:15:26 +0700

    feat: init project

---
+++ FFNN.py
@@ -0,0 +1,25 @@
+
+class FeedForwardNeuralNetwork:
+    def __init__(self) -> None:
+        pass 
+
+    def loadModel()-> None:
+        pass 
+    
+    def printModel()-> None:
+        pass 
+
+    def predict()-> None:
+        pass 
+
+    def sigmoid()-> None:
+        pass 
+
+    def linear()-> None:
+        pass 
+
+    def reLu()-> None:
+        pass 
+
+    def softmax()-> None:
+        pass 
